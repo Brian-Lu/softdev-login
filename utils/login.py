e62e70d2ad74bff784c2b212f686598f6cd0eb03
Brian-Lu <brianlu22@yahoo.com>
2016-10-06 04:01:25 -0400

    Fixed login

--- utils/login.py
+++ utils/login.py
@@ -4,8 +4,9 @@ def login(username, password):
     data = open("data/login.csv", "r")
     for line in data:
         indexOfPassword = line.find(',')
-        if line[:indexOfPassword] == username and line[indexOfPassword + 1:] == hashlib.sha224(password).hexdigest():
+        if line[:indexOfPassword] == username and line[indexOfPassword + 1:] == hashlib.sha224(password).hexdigest() + '\n':
             return "You have successfully logged in, " + username
         elif line[:indexOfPassword] == username:
             return "Wrong password??? Try again!!!"
     return "Wrong username??? Try again!!!"
+
